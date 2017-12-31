96d5556db2568eda8453cd3d1a8ee2b231989533
João Luís Reis <jype_13@hotmail.com>
2017-12-31 17:35:07 +0000

    attempt

--- index.php
+++ index.php
@@ -1,315 +1,199 @@
-<?php
-/**
- * CodeIgniter
- *
- * An open source application development framework for PHP
- *
- * This content is released under the MIT License (MIT)
- *
- * Copyright (c) 2014 - 2017, British Columbia Institute of Technology
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- *
- * @package	CodeIgniter
- * @author	EllisLab Dev Team
- * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (https://ellislab.com/)
- * @copyright	Copyright (c) 2014 - 2017, British Columbia Institute of Technology (http://bcit.ca/)
- * @license	http://opensource.org/licenses/MIT	MIT License
- * @link	https://codeigniter.com
- * @since	Version 1.0.0
- * @filesource
- */
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <meta name="description" content="">
+        <meta name="author" content="">
 
-/*
- *---------------------------------------------------------------
- * APPLICATION ENVIRONMENT
- *---------------------------------------------------------------
- *
- * You can load different configurations depending on your
- * current environment. Setting the environment also influences
- * things like logging and error reporting.
- *
- * This can be set to anything, but default usage is:
- *
- *     development
- *     testing
- *     production
- *
- * NOTE: If you change these, also change the error_reporting() code below
- */
-	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+        <title>João Luís Reis</title>
 
-/*
- *---------------------------------------------------------------
- * ERROR REPORTING
- *---------------------------------------------------------------
- *
- * Different environments will require different levels of error reporting.
- * By default development will show errors but testing and live will hide them.
- */
-switch (ENVIRONMENT)
-{
-	case 'development':
-		error_reporting(-1);
-		ini_set('display_errors', 1);
-	break;
+        <!-- Bootstrap core CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-	case 'testing':
-	case 'production':
-		ini_set('display_errors', 0);
-		if (version_compare(PHP_VERSION, '5.3', '>='))
-		{
-			error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
-		}
-		else
-		{
-			error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE);
-		}
-	break;
+        <!-- Custom CSS -->
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet"/>
 
-	default:
-		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
-		echo 'The application environment is not set correctly.';
-		exit(1); // EXIT_ERROR
-}
+        <link href="/css/main.css" rel="stylesheet" type="text/css"/>
 
-/*
- *---------------------------------------------------------------
- * SYSTEM DIRECTORY NAME
- *---------------------------------------------------------------
- *
- * This variable must contain the name of your "system" directory.
- * Set the path if it is not in the same directory as this file.
- */
-	$system_path = 'system';
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
 
-/*
- *---------------------------------------------------------------
- * APPLICATION DIRECTORY NAME
- *---------------------------------------------------------------
- *
- * If you want this front controller to use a different "application"
- * directory than the default one you can set its name here. The directory
- * can also be renamed or relocated anywhere on your server. If you do,
- * use an absolute (full) server path.
- * For more info please see the user guide:
- *
- * https://codeigniter.com/user_guide/general/managing_apps.html
- *
- * NO TRAILING SLASH!
- */
-	$application_folder = 'application';
+        <script src="/js/less.js" type="text/javascript"></script>
+        <script src="/js/form.js" type="text/javascript"></script>
 
-/*
- *---------------------------------------------------------------
- * VIEW DIRECTORY NAME
- *---------------------------------------------------------------
- *
- * If you want to move the view directory out of the application
- * directory, set the path to it here. The directory can be renamed
- * and relocated anywhere on your server. If blank, it will default
- * to the standard location inside your application directory.
- * If you do move this, use an absolute (full) server path.
- *
- * NO TRAILING SLASH!
- */
-	$view_folder = '';
+    </head>
+
+    <body>
+		<div class="container">
+
+			<!-- Page Heading -->
+			<div class="row">
+				<div class="col-lg-12">
+					<h1 class="page-header">Work Projects
+					</h1>
+				</div>
+			</div>
+			<!-- /.row -->
+
+			<!-- Projects Row -->
+			<div class="row">
+				<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 portfolio-item">
+					<div class="container_box">
+						<div class="main_page_container">
+							<a href="https://www.worldwaronline.com/" target="_blank">
+								<div class="main_page_image" style="background-image: url('../media/ray_tracer/scrnsht_final.jpg')"></div>
+							</a>
+						</div>
+					</div>
+					<h2><a href="https://www.worldwaronline.com/" target="_blank"> World War Online </a></h2>
+					<h4> Mar 2016 - Jun 2017 <a href="http://www.chilltime.com/" target="_blank">[Chilltime]</a></h4>
+					<p> Developed and maintained core features for the company's main product, both front-end, using HTML5, CSS3 and Javascript, and back-end, using PHP. My main contributions were the development of a new, more dynamic battle system and a community based translation system. Version control was done using SVN tailored to the company's needs and product development management was done through SCRUM. Worked directly with the development, design and community management teams.
+
+					</p>
+				</div>
+			</div>
+			<!-- /.row -->
+
+			<!-- Page Heading -->
+			<div class="row">
+				<div class="col-lg-12">
+					<h1 class="page-header">Projects
+					</h1>
+				</div>
+			</div>
+			<!-- /.row -->
+
+			<!-- Projects Row -->
+			<div class="row">
+				<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 portfolio-item">
+					<div class="container_box">
+						<div class="main_page_container">
+							<a href="https://ldjam.com/events/ludum-dare/40/loot-grabber" target="_blank">
+								<div class="main_page_image" style="background-image: url('../media/ray_tracer/scrnsht_final.jpg')"></div>
+							</a>
+						</div>
+					</div>
+					<h2><a href="https://ldjam.com/events/ludum-dare/40/loot-grabber" target="_blank"> Loot Grabber </a></h2>
+					<h4> 2017</h4>
+					<p> Development of a 3D top-down perspective twin-stick bullet-hell game as a group of 2 programmers for the Ludum Dare 40 Jam and we created all code and assets during the event.
+						We used Unity3D, Blender, Microsoft Visual Studio and Git.
+
+					</p>
+				</div>
+				<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 portfolio-item">
+					<div class="container_box">
+						<div class="main_page_container">
+							<a href="https://globalgamejam.org/2017/games/rubber-ducky-league" target="_blank">
+								<div class="main_page_image" style="background-image: url('../media/ray_tracer/scrnsht_final.jpg')"></div>
+							</a>
+						</div>
+					</div>
+					<h2><a href="https://globalgamejam.org/2017/games/rubber-ducky-league" target="_blank"> Rubber Ducky League </a></h2>
+					<h4> 2017</h4>
+					<p> University project. As a group, we created a simulation with reactive and deliberative AI agents of firefighters putting out fires throughout a variable sized city.
+						Developed using Unity3D and Microsoft Visual Studio, programmed in C#. For version control, we used Git.
+
+					</p>
+				</div>
+			</div>
+			<!-- /.row -->
+
+			<!-- Page Heading -->
+			<div class="row">
+				<div class="col-lg-12">
+					<h1 class="page-header">University Projects
+					</h1>
+				</div>
+			</div>
+			<!-- /.row -->
+
+			<!-- Projects Row -->
+			<div class="row">
+				<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 portfolio-item">
+					<div class="container_box">
+						<div class="main_page_container">
+							<a href="<?php echo site_url();?>">
+								<div class="main_page_image" style="background-image: url('../media/ray_tracer/scrnsht_final.jpg')"></div>
+							</a>
+						</div>
+					</div>
+					<h2> ELEMENTIUM </h2>
+					<h4> 2014 - 2016</h4>
+					<p> Development of a 2D top-down perspective action roguelike game as a group of 3 programmers. We started by testing out mechanics that were going to be used, with users, and developed a prototype in GameMaker Studio. Then we developed the game in Unity3D and got user feedback at the university by letting the public play in a game projects show.
+						<br>
+						We wanted to achieve a fast paced game with elements of metroidvania and roguelike and randomly generated maps so that every experience would be different. My main contributions were towards UI interaction and character abilities' interaction with other game elements.
+						The development was done using Unity3D and Microsoft Visual Studio, programming in C#. For version control, we used Git.
+
+					</p>
+				</div>
+				<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 portfolio-item">
+					<div class="container_box">
+						<div class="main_page_container">
+							<a href="<?php echo site_url();?>">
+								<div class="main_page_image" style="background-image: url('../media/ray_tracer/scrnsht_final.jpg')"></div>
+							</a>
+						</div>
+					</div>
+					<h2> Firefighters </h2>
+					<h4> 2014 - 2015</h4>
+					<p> University project. As a group, we created a simulation with reactive and deliberative AI agents of firefighters putting out fires throughout a variable sized city.
+						Developed using Unity3D and Microsoft Visual Studio, programmed in C#. For version control, we used Git.
+
+					</p>
+				</div>
+			</div>
+			<!-- /.row -->
+
+			<!-- Projects Row -->
+			<div class="row">
+				<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 portfolio-item">
+					<div class="container_box">
+						<div class="main_page_container">
+							<a href="<?php echo site_url();?>">
+								<div class="main_page_image" style="background-image: url('../media/ray_tracer/scrnsht_final.jpg')"></div>
+							</a>
+						</div>
+					</div>
+					<h2> Ray Tracer </h2>
+					<h4> 2014</h4>
+					<p> University project. Development of a ray tracer using anti-aliasing (Monte Carlo sampling), soft shadows, depth
+						of field and acceleration structures. C++ application.
+
+					</p>
+				</div>
+				<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 portfolio-item">
+					<div class="container_box">
+						<div class="main_page_container">
+							<a href="<?php echo site_url();?>">
+								<div class="main_page_image" style="background-image: url('../media/ray_tracer/scrnsht_final.jpg')"></div>
+							</a>
+						</div>
+					</div>
+					<h2> OpenGL Modelling Engine </h2>
+					<h4> 2014</h4>
+					<p> University project. Using modern OpenGL and GLSL shaders, we developed a C++ application which allowed the user to import .obj with or without textures and to translate, rotate and scale those objects.
+						The user could also apply simple glass or perlin noise shaders to those objects.
+
+					</p>
+				</div>
+			</div>
+			<!-- /.row -->
+
+		</div>
+
+        <!-- Bootstrap core JavaScript
+        ================================================== -->
+        <!-- Placed at the end of the document so the pages load faster -->
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="/bootstrap/js/bootstrap.min.js"></script>
 
 
-/*
- * --------------------------------------------------------------------
- * DEFAULT CONTROLLER
- * --------------------------------------------------------------------
- *
- * Normally you will set your default controller in the routes.php file.
- * You can, however, force a custom routing by hard-coding a
- * specific controller class/function here. For most applications, you
- * WILL NOT set your routing here, but it's an option for those
- * special instances where you might want to override the standard
- * routing in a specific front controller that shares a common CI installation.
- *
- * IMPORTANT: If you set the routing here, NO OTHER controller will be
- * callable. In essence, this preference limits your application to ONE
- * specific controller. Leave the function name blank if you need
- * to call functions dynamically via the URI.
- *
- * Un-comment the $routing array below to use this feature
- */
-	// The directory name, relative to the "controllers" directory.  Leave blank
-	// if your controller is not in a sub-directory within the "controllers" one
-	// $routing['directory'] = '';
-
-	// The controller class file name.  Example:  mycontroller
-	// $routing['controller'] = '';
-
-	// The controller function you wish to be called.
-	// $routing['function']	= '';
-
-
-/*
- * -------------------------------------------------------------------
- *  CUSTOM CONFIG VALUES
- * -------------------------------------------------------------------
- *
- * The $assign_to_config array below will be passed dynamically to the
- * config class when initialized. This allows you to set custom config
- * items or override any default config values found in the config.php file.
- * This can be handy as it permits you to share one application between
- * multiple front controller files, with each file containing different
- * config values.
- *
- * Un-comment the $assign_to_config array below to use this feature
- */
-	// $assign_to_config['name_of_config_item'] = 'value of config item';
-
-
-
-// --------------------------------------------------------------------
-// END OF USER CONFIGURABLE SETTINGS.  DO NOT EDIT BELOW THIS LINE
-// --------------------------------------------------------------------
-
-/*
- * ---------------------------------------------------------------
- *  Resolve the system path for increased reliability
- * ---------------------------------------------------------------
- */
-
-	// Set the current directory correctly for CLI requests
-	if (defined('STDIN'))
-	{
-		chdir(dirname(__FILE__));
-	}
-
-	if (($_temp = realpath($system_path)) !== FALSE)
-	{
-		$system_path = $_temp.DIRECTORY_SEPARATOR;
-	}
-	else
-	{
-		// Ensure there's a trailing slash
-		$system_path = strtr(
-			rtrim($system_path, '/\\'),
-			'/\\',
-			DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
-		).DIRECTORY_SEPARATOR;
-	}
-
-	// Is the system path correct?
-	if ( ! is_dir($system_path))
-	{
-		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
-		echo 'Your system folder path does not appear to be set correctly. Please open the following file and correct this: '.pathinfo(__FILE__, PATHINFO_BASENAME);
-		exit(3); // EXIT_CONFIG
-	}
-
-/*
- * -------------------------------------------------------------------
- *  Now that we know the path, set the main path constants
- * -------------------------------------------------------------------
- */
-	// The name of THIS file
-	define('SELF', pathinfo(__FILE__, PATHINFO_BASENAME));
-
-	// Path to the system directory
-	define('BASEPATH', $system_path);
-
-	// Path to the front controller (this file) directory
-	define('FCPATH', dirname(__FILE__).DIRECTORY_SEPARATOR);
-
-	// Name of the "system" directory
-	define('SYSDIR', basename(BASEPATH));
-
-	// The path to the "application" directory
-	if (is_dir($application_folder))
-	{
-		if (($_temp = realpath($application_folder)) !== FALSE)
-		{
-			$application_folder = $_temp;
-		}
-		else
-		{
-			$application_folder = strtr(
-				rtrim($application_folder, '/\\'),
-				'/\\',
-				DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
-			);
-		}
-	}
-	elseif (is_dir(BASEPATH.$application_folder.DIRECTORY_SEPARATOR))
-	{
-		$application_folder = BASEPATH.strtr(
-			trim($application_folder, '/\\'),
-			'/\\',
-			DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
-		);
-	}
-	else
-	{
-		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
-		echo 'Your application folder path does not appear to be set correctly. Please open the following file and correct this: '.SELF;
-		exit(3); // EXIT_CONFIG
-	}
-
-	define('APPPATH', $application_folder.DIRECTORY_SEPARATOR);
-
-	// The path to the "views" directory
-	if ( ! isset($view_folder[0]) && is_dir(APPPATH.'views'.DIRECTORY_SEPARATOR))
-	{
-		$view_folder = APPPATH.'views';
-	}
-	elseif (is_dir($view_folder))
-	{
-		if (($_temp = realpath($view_folder)) !== FALSE)
-		{
-			$view_folder = $_temp;
-		}
-		else
-		{
-			$view_folder = strtr(
-				rtrim($view_folder, '/\\'),
-				'/\\',
-				DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
-			);
-		}
-	}
-	elseif (is_dir(APPPATH.$view_folder.DIRECTORY_SEPARATOR))
-	{
-		$view_folder = APPPATH.strtr(
-			trim($view_folder, '/\\'),
-			'/\\',
-			DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
-		);
-	}
-	else
-	{
-		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
-		echo 'Your view folder path does not appear to be set correctly. Please open the following file and correct this: '.SELF;
-		exit(3); // EXIT_CONFIG
-	}
-
-	define('VIEWPATH', $view_folder.DIRECTORY_SEPARATOR);
-
-/*
- * --------------------------------------------------------------------
- * LOAD THE BOOTSTRAP FILE
- * --------------------------------------------------------------------
- *
- * And away we go...
- */
-require_once BASEPATH.'core/CodeIgniter.php';
+    </body>
+</html>
